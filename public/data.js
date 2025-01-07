8a6ffdff3ecabaa6867d65aab42ed4eb28ff4071
ethanlaw <ethan@bitlore.io>
2025-01-07 14:54:57 -0800

    Enable HMTL within text data

---
+++ public/data.js
@@ -0,0 +1,7 @@
+export const pageData = {
+    hero: {
+        headline: "Hi, my name is <span className='font-bold'>Ethan</span>",
+        subheadline:
+            "I am a front&nbsp;end <span className='font-bold'>web&nbsp;developer</span> from <span class='font-bold'>Portland,&nbsp;OR</span>",
+    },
+};
