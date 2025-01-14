4bff853968a4dcd1ef05e1920ab383f66b501c92
ethanlaw <ethan@bitlore.io>
2025-01-14 15:03:24 -0800

    Hero formatting

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -10,24 +10,22 @@ function Hero() {
 
     return (
         <section className={sectionClasses}>
-            <div className='flex flex-col justify-between md:h-3/4 z-20'>
+            <div className="flex flex-col justify-between md:h-3/4 z-20">
                 <div>
-                    <h1 className='text-5xl md:text-7xl leading-tight md:leading-tight text-outline'>
-                        {parse(headline)}
-                    </h1>
-                    <h2 className='text-3xl mt-5'>{parse(subheadline)}</h2>
+                    <h1>{parse(headline)}</h1>
+                    <p className="text-3xl mt-5">{parse(subheadline)}</p>
                 </div>
-                <div className='self-start'>
-                    <Scroller />
+                <div className="self-start">
+                    <Scroller target="work-section" />
                 </div>
             </div>
-            <div className='relative self-stretch flex flex-col'>
+            <div className="relative self-stretch flex flex-col">
                 <img
-                    src='/public/img/portfolio-avatar.jpeg'
-                    alt='hero image'
-                    className='relative rounded-2xl object-fit max-h-full my-auto z-10'
+                    src="/public/img/portfolio-avatar.jpeg"
+                    alt="hero image"
+                    className="relative rounded-2xl object-fit max-h-full my-auto z-10"
                 />
-                <Checkerboard classes='absolute h-[550px] w-80 top-32 -left-[190px] rounded-2xl bg-[length:100px]' />
+                <Checkerboard classes="absolute h-[550px] w-80 top-32 -left-[190px] rounded-2xl bg-[length:100px]" />
             </div>
         </section>
     );
