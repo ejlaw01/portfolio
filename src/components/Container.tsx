3b91f23174e55060a2fe0282a17ddfb52ad6c504
ethanlaw <ethan@bitlore.io>
2025-01-30 14:48:30 -0800

    Prettier settings

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,12 +1,12 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 
 type ContainerProps = {
     children: ReactNode;
     classes?: string;
-}
+};
 
 const Container = ({ children, classes }: ContainerProps) => {
-    const containerClasses : string = `container${classes ? " " + classes : ""}`;
+    const containerClasses: string = `container${classes ? " " + classes : ""}`;
     return <div className={containerClasses}>{children}</div>;
 };
 
